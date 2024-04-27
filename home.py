c124a993ed7eb87460dc1b1ef81b8111c90ce72f
Samir Tarda <115050984+sntarda@users.noreply.github.com>
2024-04-27 05:59:01 -0500

    Update home.py

--- home.py
+++ home.py
@@ -58,7 +58,7 @@ def main(st):
     st.image('data-analytics1.jpg',  width=700, output_format='auto')
     st.image('data-analytics2.png',  width=700, output_format='auto')
     <div style='text-align: left;'>
-        <h3>WORKFLOW DIAGRAM</h3>
+        <h3>WORKFLOW CHART</h3>
     </div>
     """, unsafe_allow_html=True)
     st.image('workflowdiagram.jpg',  width=700, output_format='auto')
